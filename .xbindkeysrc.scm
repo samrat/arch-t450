0e8cf90420b41b0d8b738c096c3f2d04c4b7107e
Samrat Man Singh <samratmansingh@gmail.com>
2017-11-19 12:25:52 +0545

    Use pamixer instead of having to use hard-coded sinks

--- .xbindkeysrc.scm
+++ .xbindkeysrc.scm
@@ -5,16 +5,19 @@
       '(XF86MonBrightnessDown . "xbacklight -dec 3")
       ;; Volume
       ;; NOTE: `pactl list sinks short` to find the sink id
-      '(XF86AudioRaiseVolume . "pactl set-sink-volume 1 +5%")
-      '(XF86AudioLowerVolume . "pactl set-sink-volume 1 -5%")
-      '(XF86AudioMute . "pactl set-sink-mute 1 toggle")
+      '(XF86AudioRaiseVolume . "pamixer --increase 5")
+      '(XF86AudioLowerVolume . "pamixer --decrease 5")
+      '(XF86AudioMute . "pamixer -t")
       ;; `pactl list sources short` to find the source id
-      '(XF86AudioMicMute . "pactl set-source-mute 2 toggle")
+      '(XF86AudioMicMute . "pactl set-source-mute 1 toggle")
       
       ;; Print screen
       '(Print . "scrot '%Y-%m-%d-%H%M%S_$wx$h.png' -e 'mv $f ~/screenshots/'")
 
       '(XF86LaunchA . "~/bin/batt-breakdown.sh")
-      '((mod4 XF86Explorer) . "i3lock")
+      '((mod4 XF86Explorer) . "i3lock && xset dpms force off")
 
+      '((mod4 XF86LaunchA) . "setxkbmap -layout us")
+      '((mod4 Shift XF86LaunchA) . "setxkbmap -layout us -variant colemak")
+      
       '((mod4 Shift b) . "~/bin/today-loadsched.sh")))
